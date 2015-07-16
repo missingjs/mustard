ce04271039ce2760ed9f7ca5be03cc8f6c0addf0
Tang Qiming <tangqiming@tigerknows.com>
2015-07-16 12:55:24 +0800

    dsb: add 2_2_12

---
+++ dsb/2/2_2_12.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include "common/array.h"
+using namespace mustard;
+
+int remove_x(int * arr, int len, int x);
+
+int main()
+{
+    int len = 0;
+    std::cin >> len;
+
+    int * arr = array_read<int>(len);
+
+    int x = 0;
+    std::cin >> x;
+
+    int new_len = remove_x(arr, len, x);
+
+    array_print(arr, new_len);
+
+    return 0;
+}
+
+int remove_x(int * arr, int len, int x)
+{
+    if (arr == NULL || len <= 0) {
+        return 0;
+    }
+
+    int k = 0;
+    for (int i = 0; i < len; ++i) {
+        if (arr[i] != x) {
+            if (k > 0) {
+                arr[i-k] = arr[i];
+            }
+        } else {
+            ++k;
+        }
+    }
+
+    return len - k;
+}
