82656819253ebc214ca1bf14aa35c17a2821efa3
Tang Qiming <tangqiming@tigerknows.com>
2015-07-27 12:03:22 +0800

    dsb: add 2-3-25

--- dsb/2/3/25.cpp
+++ dsb/2/3/25.cpp
@@ -1,1 +1,21 @@
 // @brief: 对带头结点的单链表进行递增排序
+
+#include <iostream>
+#include "common/slist.h"
+using namespace mustard;
+
+void slist_sort(slist::node<int> * head);
+
+int main()
+{
+    slist::node<int> * front = slist::read<int>();
+    slist::node<int> head;
+    head.next = front;
+
+    slist_sort(&head);
+
+    slist::print(head.next);
+    slist::free(head.next);
+
+    return 0;
+}
