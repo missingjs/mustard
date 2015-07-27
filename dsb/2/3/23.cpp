a34dfc71b3109032f93617f33aa672a27446d942
Tang Qiming <tangqiming@tigerknows.com>
2015-07-27 11:22:00 +0800

    dsb: add 2-3-23

--- dsb/2/3/23.cpp
+++ dsb/2/3/23.cpp
@@ -1,3 +1,18 @@
 // @brief: 删除非有序单链表中值域重复的结点
 
+#include "common/slist.h"
+using namespace mustard;
 
+void remove_dup_unseq(slist::node<int> * h);
+
+int main()
+{
+    slist::node<int> * h = slist::read<int>();
+
+    remove_dup_unseq(h);
+
+    slist::print(h);
+    slist::free(h);
+
+    return 0;
+}
