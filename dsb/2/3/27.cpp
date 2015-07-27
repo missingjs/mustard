c3f5dd5b21fd98e17d2dac72ab8b40756e891687
Tang Qiming <tangqiming@tigerknows.com>
2015-07-27 14:35:33 +0800

    dsb: add 2-3-27

--- dsb/2/3/27.cpp
+++ dsb/2/3/27.cpp
@@ -1,1 +1,21 @@
 // @brief: 将带头结点单链表逆置，空间复杂度为O(1)
+
+#include "common/slist.h"
+using namespace mustard;
+
+void reverse(slist::node<int> * head);
+
+int main()
+{
+    slist::node<int> * front = slist::read<int>();
+
+    slist::node<int> head;
+    head.next = front;
+
+    reverse(&head);
+
+    slist::print(head.next);
+    slist::free(head.next);
+
+    return 0;
+}
