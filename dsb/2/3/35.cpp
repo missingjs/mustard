5a44986fe029509e6fe3c586dc3dca2c0da19316
Qiming Tang <venux021@outlook.com>
2015-07-27 23:38:40 +0800

    dsb: 2-3-35

--- dsb/2/3/35.cpp
+++ dsb/2/3/35.cpp
@@ -1,1 +1,21 @@
 // @brief: 三个非递减有序（可能存在重复值）单链表ABC，使A中留下3表中均包含值的结点，释放其它结点，限定算法时间复杂度为O(|A|+|B|+|C|)
+#include "common/slist.h"
+using namespace mustard;
+
+typedef slist::node<int> node_t;
+
+node_t * unify(node_t * a, node_t * b, node_t * c);
+
+int main()
+{
+    node_t * a = slist::read<int>();
+    node_t * b = slist::read<int>();
+    node_t * c = slist::read<int>();
+
+    a = unify(a, b, c);
+    slist::print(a);
+
+    slist::free(a);
+
+    return 0;
+}
