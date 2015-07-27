0d717d7aa42ca1d781eae9b3fed7f1ccfa7db6cf
Tang Qiming <tangqiming@tigerknows.com>
2015-07-27 14:24:10 +0800

    dsb: add 2-3-26

--- dsb/2/3/26.cpp
+++ dsb/2/3/26.cpp
@@ -1,1 +1,21 @@
 // @brief: 从带头结点的单链表中删除一个最小值结点
+
+#include "common/slist.h"
+using namespace mustard;
+
+void remove_min(slist::node<int> * head);
+
+int main()
+{
+    slist::node<int> * front = slist::read<int>();
+
+    slist::node<int> head;
+    head.next = front;
+
+    remove_min(&head);
+
+    slist::print(head.next);
+    slist::free(head.next);
+
+    return 0;
+}
