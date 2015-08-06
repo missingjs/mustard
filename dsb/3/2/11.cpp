90663bc2e897f5d5b9a214963fc554076e474910
Qiming Tang <tangqiming@tigerknows.com>
2015-08-06 12:27:50 +0800

    dsb: add 3-2-11

--- dsb/3/2/11.cpp
+++ dsb/3/2/11.cpp
@@ -5,16 +5,17 @@ using namespace mustard;
 
 typedef mtx::matrix<int> matrix_t;
 
-void print_path(matrix_t * m, int start_row, int start_col);
+// 提供起点和终点
+void print_path(matrix_t * m, int br, int bc, int er, int ec);
 
 int main()
 {
     matrix_t * m = mtx::read<int>();
 
-    int sr = 0, sc = 0;
-    std::cin >> sr >> sc;
+    int br = 0, bc = 0, er = 0, ec = 0;
+    std::cin >> br >> bc >> er >> ec;
 
-    print_path(m, sr, sc);
+    print_path(m, br, bc, er, ec);
 
     mtx::free(m);
 
--- dsb/3/2/11.sample.cpp
+++ dsb/3/2/11.sample.cpp
@@ -1,4 +1,5 @@
 #include <stack>
+#include <list>
 #include "common/matrix.h"
 using namespace mustard;
 
@@ -14,18 +15,79 @@ struct mz
     {}
 };
 
-void print_path(matrix_t * m, int start_row, int start_col)
+bool is_in(matrix_t * m, int r, int c)
 {
-    if (m == NULL || start_row < 0 || start_row >= m->row || start_col < 0 || start_col >= m->col || m->get(start_row, start_col) != 0) {
+    return r >= 0 && r < m->row && c >= 0 && c < m->col;
+}
+
+void dump_path(std::stack<mz> & stk, std::list<mz> & path)
+{
+    path.clear();
+
+    while (!stk.empty()) {
+        path.push_front(stk.top());
+        stk.pop();
+    }
+
+    for (std::list<mz>::iterator i = path.begin(); i != path.end(); ++i) {
+        stk.push(*i);
+    }
+}
+
+void print_path(const std::list<mz> & path)
+{
+    std::cout << "path => " << path.size() << '\n';
+    int c = 0;
+    for (std::list<mz>::const_iterator i = path.begin(); i != path.end(); ++i) {
+        std::cout << '(' << i->r << ',' << i->c << ") ";
+        ++c;
+        if (c % 5 == 0) {
+            std::cout << '\n';
+        }
+    }
+
+    std::cout << '\n';
+    if (c % 5) {
+        std::cout << '\n';
+    }
+}
+
+void print_path(std::stack<mz> & stk)
+{
+    std::list<mz> path;
+    dump_path(stk, path);
+    print_path(path);
+}
+
+void print_path(matrix_t * m, int br, int bc, int er, int ec)
+{
+    if (m == NULL || !is_in(m,br,bc) || !is_in(m,er,ec) 
+            || m->get(br, bc) != 0 || m->get(er,ec) != 0) 
+    {
         return;
     }
 
     std::stack<mz> stk;
-    stk.push(mz(start_row, start_col));
+    std::list<mz> path;
+
+    m->set(br,bc,-1);
+    stk.push(mz(br,bc));
 
     while (!stk.empty()) {
         mz & top = stk.top();
+        if (top.r == er && top.c == ec) {
+            if (path.size() == 0 || stk.size() < path.size()) {
+                dump_path(stk, path);
+                print_path(path);
+            } else {
+                print_path(stk);
+            }
+            m->set(top.r, top.c, 0);
+            stk.pop();
+            continue;
+        }
         if (top.dir > 3) {
+            m->set(top.r, top.c, 0);
             stk.pop();
             continue;
         }
@@ -48,5 +110,13 @@ void print_path(matrix_t * m, int start_row, int start_col)
                 n_c = top.c - 1;
                 break;
         }
+        ++top.dir;
+        if (m->get(n_r,n_c) == 0) {
+            m->set(n_r,n_c,-1);
+            stk.push(mz(n_r,n_c));
+        }
     }
+
+    std::cout << "the shortest path: \n";
+    print_path(path);
 }
