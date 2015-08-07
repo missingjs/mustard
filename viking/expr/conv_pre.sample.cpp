e0c5b72ae90278544eb9a930e06d1a80446178a0
Qiming Tang <tangqiming@tigerknows.com>
2015-08-07 18:05:14 +0800

    viking: expr, 从波兰式序列构造语法树，将原递归算法改写为非递归

--- viking/expr/conv_pre.sample.cpp
+++ viking/expr/conv_pre.sample.cpp
@@ -27,12 +27,66 @@ expr_node * make_tree_1(const std::vector<token> & tokens)
     return mt1(i);
 }
 
+#include <stack>
+
+struct mt_node
+{
+    expr_node * * ret;
+    int state;
+
+    char op;
+    expr_node *e1, *e2;
+
+    mt_node(expr_node ** ret)
+        : ret(ret), state(0), op(0), e1(NULL), e2(NULL)
+    {}
+};
+
+expr_node * make_tree_2(const std::vector<token> & tokens)
+{
+    cvi_t i = tokens.begin();
+    expr_node * root = NULL;
+
+    std::stack<mt_node> stk;
+    stk.push(mt_node(&root));
+
+    while (!stk.empty()) {
+        mt_node & top = stk.top();
+        switch (top.state) {
+            case 0:
+                if (i->type == token::NUM) {
+                    int num = i->d.num;
+                    ++i;
+                    *top.ret = new expr_node(num);
+                    stk.pop();
+                } else {
+                    top.op = i->d.op;
+                    ++i;
+                    top.state = 1;
+                    stk.push(mt_node(&top.e1));
+                }
+                break;
+            case 1:
+                top.state = 2;
+                stk.push(mt_node(&top.e2));
+                break;
+            case 2:
+                *top.ret = new expr_node(top.op, top.e1, top.e2);
+                stk.pop();
+                break;
+        }
+    }
+
+    return root;
+}
+
 void conv_pre(const char * expr, std::string & in, std::string & post)
 {
     std::vector<token> tokens;
     parse(expr, tokens);
 
-    expr_node * root = make_tree_1(tokens);
+    // expr_node * root = make_tree_1(tokens);
+    expr_node * root = make_tree_2(tokens);
 
     dump_in(root, in);
     dump_post(root, post);
