12e71acbce7d2575429ef271df51415fd8c44105
Qiming Tang <tangqiming@tigerknows.com>
2015-08-07 15:15:28 +0800

    viking: expr, post-suffix evaluation finished

--- viking/expr/expr.sample.cpp
+++ viking/expr/expr.sample.cpp
@@ -211,6 +211,27 @@ int eval_in(const std::vector<token> & _tokens)
     return num_stk.top();
 }
 
+int eval_post(const std::vector<token> & tokens)
+{
+    std::vector<token>::const_iterator i = tokens.begin();
+
+    std::stack<int> num_stk;
+    for (; i != tokens.end(); ++i) {
+        if (i->type == token::NUM) {
+            num_stk.push(i->d.num);
+        } else {
+            int n2 = num_stk.top();
+            num_stk.pop();
+            int n1 = num_stk.top();
+            num_stk.pop();
+            int value = eval(n1, i->d.op, n2);
+            num_stk.push(value);
+        }
+    }
+
+    return num_stk.top();
+}
+
 void expr_process(const char * type, const char * expr)
 {
     std::vector<token> tokens;
@@ -222,7 +243,7 @@ void expr_process(const char * type, const char * expr)
     } else if (strcmp(type, "IN") == 0) {
         value = eval_in(tokens);
     } else if (strcmp(type, "POST") == 0) {
-        //value = eval_post(tokens);
+        value = eval_post(tokens);
     } else {
         std::cout << "unknown type: " << type << '\n';
         return;
