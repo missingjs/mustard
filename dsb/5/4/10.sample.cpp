089a92355ee1782c731cade1a270346ab2a6e371
Qiming Tang <venux021@outlook.com>
2015-08-16 00:51:39 +0800

    dsb: 5-4-10, add new solution, using suffix array

--- dsb/5/4/10.sample.cpp
+++ dsb/5/4/10.sample.cpp
@@ -1,5 +1,8 @@
 #include <cstddef>
 #include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
 
 char * f1(const char * str)
 {
@@ -21,10 +24,9 @@ char * f1(const char * str)
                     index = i;
                     length = length1;
                 }
-                j += length1;
-            } else {
-                ++j;
+                // j += length1;
             }
+            ++j;
         }
         ++i;
     }
@@ -63,7 +65,47 @@ char * f2(const char * str)
     return r;
 }
 
+int _cmp(const void * p1, const void * p2)
+{
+    return strcmp(*(const char **)p1, *(const char **)p2);
+}
+
+struct c_cmp
+{
+    bool operator() (const char * p1, const char * p2)
+    {
+        return strcmp(p1, p2) < 0;
+    }
+};
+
+char * f3(const char * str)
+{
+    int len = (int) strlen(str);
+    const char ** suff = new const char* [len];
+
+    for (int i = 0; i < len; ++i) {
+        suff[i] = str + i;
+    }
+
+    // qsort(suff, len, sizeof(const char *), _cmp);
+    std::sort(suff, suff + len, c_cmp());
+
+    int max_len = 0, index = 0;
+    for (int i = 0; i < len - 1; ++i) {
+        int len = do_cmp(suff[i], suff[i+1]);
+        if (len > max_len) {
+            max_len = len;
+            index = i;
+        }
+    }
+
+    char * t = new char[max_len + 1];
+    strncpy(t, suff[index], max_len);
+    t[max_len] = 0;
+    return t;
+}
+
 char * longest_repeat_str(const char * str)
 {
-    return f1(str);
+    return f3(str);
 }
