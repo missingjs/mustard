c742c935cb72785ee755a09d2a1b95f7a5a43a08
Qiming Tang <tangqiming@tigerknows.com>
2015-08-18 19:46:34 +0800

    common: add sparse matrix

---
+++ common/__sparse_matrix_impl.h
@@ -0,0 +1,252 @@
+#include <algorithm>
+
+namespace mustard {
+
+namespace matrix {
+
+template <typename T>
+sparse_matrix<T>::tup::tup(int r, int c, const T & d)
+    : r(r), c(c), d(d), next(NULL)
+{
+}
+
+template <typename T>
+const T sparse_matrix<T>::_default_value = T();
+
+template <typename T>
+sparse_matrix<T>::sparse_matrix(int row, int col)
+    : _data(), _row(row), _col(col), _num_elements(0)
+{
+}
+
+template <typename T>
+sparse_matrix<T>::~sparse_matrix()
+{
+    tup * p = _data.next;
+    while (p) {
+        tup * t = p;
+        p = p->next;
+        delete t;
+    }
+}
+
+template <typename T>
+sparse_matrix<T>::sparse_matrix(const sparse_matrix<T> & m)
+    : _data(), _row(m._row), _col(m._col), _num_elements(m._num_elements)
+{
+    tup *tail = &_data, *p = m._data.next;
+    while (p) {
+        tup * n = new tup(p->r, p->c, p->d);
+        tail->next = n;
+        tail = n;
+        p = p->next;
+    }
+}
+
+template <typename T>
+sparse_matrix<T> & sparse_matrix<T>::operator =(const sparse_matrix<T> & m)
+{
+    sparse_matrix<T> tmp(m);
+    swap(tmp);
+    return (*this);
+}
+
+template <typename T>
+void sparse_matrix<T>::swap(sparse_matrix<T> & m)
+{
+    std::swap(_data, m._data);
+    std::swap(_row, m._row);
+    std::swap(_col, m._col);
+    std::swap(_num_elements, m._num_elements);
+}
+
+template <typename T>
+sparse_matrix<T>::_elem_proxy::_elem_proxy(
+        int row, int col, sparse_matrix<T> & mx)
+    : _row(row), _col(col), _mx(mx)
+{
+}
+
+template <typename T>
+sparse_matrix<T>::_elem_proxy::_elem_proxy(
+        const _elem_proxy & e)
+    : _row(e._row), _col(e._col), _mx(e._mx)
+{
+}
+
+template <typename T>
+typename sparse_matrix<T>::_elem_proxy &
+sparse_matrix<T>::_elem_proxy::operator =(const T & d)
+{
+    _mx.set(_row, _col, d);
+}
+
+template <typename T>
+sparse_matrix<T>::_elem_proxy::operator T() const
+{
+    return _mx.get(_row, _col);
+}
+
+template <typename T>
+sparse_matrix<T>::_matrix_row::_matrix_row(int row, sparse_matrix<T> & mx)
+    : _row(row), _mx(mx)
+{
+}
+
+template <typename T>
+sparse_matrix<T>::_matrix_row::_matrix_row(const _matrix_row & r)
+    : _row(r._row), _mx(r._mx)
+{
+}
+
+template <typename T>
+typename sparse_matrix<T>::_elem_proxy
+sparse_matrix<T>::_matrix_row::operator [](int i)
+{
+    return _elem_proxy(_row, i, _mx);
+}
+
+template <typename T>
+sparse_matrix<T>::_const_matrix_row::_const_matrix_row(
+        int row, const sparse_matrix<T> & mx)
+    : _row(row), _mx(mx)
+{
+}
+
+template <typename T>
+sparse_matrix<T>::_const_matrix_row::_const_matrix_row(
+        const _const_matrix_row & r)
+    : _row(r._row), _mx(r._mx)
+{
+}
+
+template <typename T>
+T sparse_matrix<T>::_const_matrix_row::operator [](int i) const
+{
+    return _mx.get(_row, i);
+}
+
+template <typename T>
+typename sparse_matrix<T>::tup *
+sparse_matrix<T>::find(int row, int col) const
+{
+    tup * p = _data.next;
+    while (p && (p->r != row || p->c != col)) {
+        p = p->next;
+    }
+    return p;
+}
+
+template <typename T>
+void sparse_matrix<T>::set(int row, int col, const T & t)
+{
+    tup * p = find(row, col);
+    if (p) {
+        p->d = t;
+    } else {
+        tup * n = new tup(row, col, t);
+        n->next = _data.next;
+        _data.next = n;
+        ++_num_elements;
+    }
+}
+
+template <typename T>
+T sparse_matrix<T>::get(int row, int col) const
+{
+    tup * p = find(row, col);
+    return p ? p->d : _default_value;
+}
+
+template <typename T>
+std::istream & operator >>(std::istream & in, sparse_matrix<T> & m)
+{
+    int ne = 0, row, col;
+    T t;
+
+    in >> ne;
+    for (int i = 0; i < ne; ++i) {
+        in >> row >> col >> t;
+        m[row][col] = t;
+    }
+}
+
+template <typename T>
+void sparse_matrix<T>::get_elements(
+        std::vector<tup> & elements) const
+{
+    tup * p = _data.next;
+    while (p) {
+        elements.push_back(*p);
+        p = p->next;
+    }
+}
+
+template <typename T>
+std::ostream & operator <<(std::ostream & out, const sparse_matrix<T> & m)
+{
+    typedef typename sparse_matrix<T>::tup tup_t;
+    std::vector<tup_t> elements;
+    m.get_elements(elements);
+    out << m.row() << ' ' << m.col() << '\n' << m.num_elements() << '\n';
+    for (size_t i = 0; i < elements.size(); ++i) {
+        out << elements[i].r << ',' << elements[i].c << ',' << elements[i].d << '\n';
+    }
+}
+
+template <typename T>
+sparse_matrix<T> * read_sparse_matrix()
+{
+    int row, col;
+    std::cin >> row >> col;
+    sparse_matrix<T> * m = new sparse_matrix<T>(row, col);
+    std::cin >> (*m);
+    return m;
+}
+
+template <typename T>
+void print(const sparse_matrix<T> * m)
+{
+    print(*m);
+}
+
+template <typename T>
+void print(const sparse_matrix<T> & m)
+{
+    std::cout << m;
+}
+
+template <typename T>
+void print_as_common(const sparse_matrix<T> * m)
+{
+    print_as_common(*m);
+}
+
+template <typename T>
+void print_as_common(const sparse_matrix<T> & m)
+{
+    for (int i = 0; i < m.row(); ++i) {
+        for (int j = 0; j < m.col(); ++j) {
+            std::cout << m[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+
+template <typename T>
+typename sparse_matrix<T>::_matrix_row
+sparse_matrix<T>::operator [](int row)
+{
+    return _matrix_row(row, *this);
+}
+
+template <typename T>
+typename sparse_matrix<T>::_const_matrix_row
+sparse_matrix<T>::operator [](int row) const
+{
+    return _const_matrix_row(row, *this);
+}
+
+} // namespace ::mustard::matrix
+
+} // namespace ::mustard
--- common/matrix2.h
+++ common/matrix2.h
@@ -2,6 +2,7 @@
 #define MUSTARD_MATRIX2_H
 
 #include <iosfwd>
+#include <vector>
 
 namespace mustard {
 
@@ -25,8 +26,6 @@ public:
     common_matrix(const common_matrix<T> & m);
 
     common_matrix<T> & operator= (const common_matrix<T> & m);
-
-    void swap(common_matrix<T> & m);
 
     int row() const { return _row; }
 
@@ -75,6 +74,8 @@ private:
 
     T d(int r, int c) const;
 
+    void swap(common_matrix<T> & m);
+
 };
 
 template <typename T>
@@ -84,7 +85,7 @@ template <typename T>
 std::ostream & operator<< (std::ostream & out, const common_matrix<T> & m);
 
 template <typename T>
-common_matrix<T> * read();
+common_matrix<T> * read_common_matrix();
 
 template <typename T>
 void print(const common_matrix<T> * m);
@@ -92,10 +93,141 @@ void print(const common_matrix<T> * m);
 template <typename T>
 void print(const common_matrix<T> & m);
 
+template <typename T>
+class sparse_matrix
+{
+public:
+    struct tup
+    {
+        int r;
+        int c;
+        T   d;
+        tup * next;
+
+        tup(int r = 0, int c = 0, const T & d = T());
+    };
+
+private:
+
+    tup _data;
+
+    int _row, _col;
+
+    int _num_elements;
+
+    static const T _default_value;
+
+public:
+
+    sparse_matrix(int row, int col);
+
+    ~sparse_matrix();
+
+    sparse_matrix(const sparse_matrix<T> & m);
+
+    sparse_matrix<T> & operator= (const sparse_matrix<T> & m);
+
+    int row() const { return _row; }
+
+    int col() const { return _col; }
+
+    int num_elements() const { return _num_elements; }
+
+    void get_elements(std::vector<tup> & elements) const;
+
+    struct _elem_proxy
+    {
+        int _row, _col;
+
+        sparse_matrix<T> & _mx;
+
+        _elem_proxy(int row, int col, sparse_matrix<T> & mx);
+
+        _elem_proxy(const _elem_proxy & e);
+
+        _elem_proxy & operator =(const T & e);
+
+        operator T() const;
+
+    private:
+        _elem_proxy & operator =(const _elem_proxy & e);
+        
+    };
+
+    struct _matrix_row
+    {
+        int _row;
+
+        sparse_matrix<T> & _mx;
+
+        _matrix_row(int row, sparse_matrix<T> & mx);
+
+        _matrix_row(const _matrix_row & r);
+
+        _elem_proxy operator[] (int i);
+
+    private:
+        _matrix_row & operator= (const _matrix_row & r);
+    };
+
+    struct _const_matrix_row
+    {
+        int _row;
+
+        const sparse_matrix<T> & _mx;
+
+        _const_matrix_row(int row, const sparse_matrix<T> & mx);
+
+        _const_matrix_row(const _const_matrix_row & r);
+
+        T operator [](int i) const;
+
+    private:
+        _const_matrix_row & operator =(const _const_matrix_row & r);
+    };
+
+    _matrix_row operator [](int row);
+
+    _const_matrix_row operator [](int row) const;
+
+private:
+
+    tup * find(int row, int col) const;
+
+    void set(int row, int col, const T & t);
+
+    T get(int row, int col) const;
+
+    void swap(sparse_matrix<T> & m);
+
+};
+
+template <typename T>
+std::istream & operator >>(std::istream & in, sparse_matrix<T> & m);
+
+template <typename T>
+std::ostream & operator <<(std::ostream & out, const sparse_matrix<T> & m);
+
+template <typename T>
+sparse_matrix<T> * read_sparse_matrix();
+
+template <typename T>
+void print(const sparse_matrix<T> * m);
+
+template <typename T>
+void print(const sparse_matrix<T> & m);
+
+template <typename T>
+void print_as_common(const sparse_matrix<T> & m);
+
+template <typename T>
+void print_as_common(const sparse_matrix<T> * m);
+
 } // namespace ::mustard::matrix
 
 } // namespace ::mustard
 
 #include "__common_matrix_impl.h"
+#include "__sparse_matrix_impl.h"
 
 #endif  // ifndef MUSTARD_MATRIX2_H
