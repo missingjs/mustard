2a6ece6122557194b93f7b026ac70f5d692396ee
Qiming Tang <tangqiming@tigerknows.com>
2015-08-25 16:15:07 +0800

    fix: some template declaration for bitree

--- common/bitree.cpp
+++ common/bitree.cpp
@@ -32,78 +32,15 @@ char _next_char(const char * & p)
 }
 
 template <>
-node<char> * read_hierarchy<char>()
+node<char> * read_hierarchy< char, node<char> >()
 {
-    /*
-    std::string input_line;
-    std::getline(std::cin, input_line);
-
-    std::stack<node<char>*> stk;
-    const char * p = input_line.c_str();
-    node<char> * nd = NULL;
-    while (*p) {
-        char ch = _next_char(p);
-        if (!ch) {
-            break;
-        }
-        switch (ch) {
-            case '(':
-                stk.push(nd);
-                nd = NULL;
-                break;
-            case ')':
-                stk.top()->rc = nd;
-                nd = stk.top();
-                stk.pop();
-                break;
-            case ',':
-                stk.top()->lc = nd;
-                nd = NULL;
-                break;
-            default:
-                nd = new node<char>(ch);
-        }
-    }
-    return nd; */
     typedef node<char> node_t;
     return _read_hierarchy_char<node_t>();
 }
 
 template <>
-threading_node<char> * read_hierarchy()
+threading_node<char> * read_hierarchy< char, threading_node<char> >()
 {
-    /*
-    std::string input_line;
-    std::getline(std::cin, input_line);
-
-    std::stack<threading_node<char>*> stk;
-    const char * p = input_line.c_str();
-    threading_node<char> * nd = NULL;
-    while (*p) {
-        char ch = _next_char(p);
-        if (!ch) {
-            break;
-        }   
-        switch (ch) {
-            case '(':
-                stk.push(nd);
-                nd = NULL;
-                break;
-            case ')':
-                stk.top()->rc = nd; 
-                nd = stk.top();
-                stk.pop();
-                break;
-            case ',':
-                stk.top()->lc = nd; 
-                nd = NULL;
-                break;
-            default:
-                nd = new threading_node<char>(
-                        _threading<char>(ch));
-        }
-    }
-    return nd; */
     typedef threading_node<char> node_t;
     return _read_hierarchy_char<node_t>();
 }
