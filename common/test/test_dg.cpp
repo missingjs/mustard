704d826445d9269d6d79af6348aee70acb7c53b7
Qiming Tang <tangqiming@tigerknows.com>
2015-08-28 11:34:36 +0800

    graph: test for directed graph using orthogonal list finished

--- common/test/test_dg.cpp
+++ common/test/test_dg.cpp
@@ -4,7 +4,8 @@ using namespace mustard;
 int main()
 {
     // typedef matrix::common_matrix<bool> st_t;
-    typedef graph::adj_list<bool> st_t;
+    // typedef graph::adj_list<bool> st_t;
+    typedef graph::orthogonal_list<bool> st_t;
     typedef graph::directed_graph<char,st_t> g_t;
 
     g_t * graph = graph::read_graph<g_t>();
