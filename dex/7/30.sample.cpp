e8fb007fa9868feb9e9392f4033ae972fa84c131
Qiming Tang <venux021@outlook.com>
2015-09-03 18:44:26 +0800

    dex: fix 7-30, print unique path using SCC

--- dex/7/30.sample.cpp
+++ dex/7/30.sample.cpp
@@ -1,5 +1,7 @@
 #include "30.def.h"
 #include <vector>
+#include <algorithm>
+#include <stack>
 
 std::vector<int> _get_indegree(list_t * L, int C)
 {
@@ -13,30 +15,65 @@ std::vector<int> _get_indegree(list_t * L, int C)
     return indeg;
 }
 
-void _dfs(graph_t * g, list_t * L, int v,
-        std::vector<int> & visited,
-        std::vector<int> & path)
+namespace {
+std::vector<int> _low, _dfn;
+std::vector<int> _visited;   // if in stack
+std::stack<int> _stk;
+std::vector<int> _scc;    // belong to a existed SCC
+int _count = 0;
+}
+
+void _dfs(graph_t * g, list_t * L, int v, std::stack<int> & path, 
+        std::vector<int> & in_path)
 {
-    visited[v] = 1;
-    path.push_back(v);
+    if (!_visited[v]) {
+        _low[v] = _dfn[v] = ++_count;
+        _stk.push(v);
+        _visited[v] = 1;
+    }
+
+    path.push(v);
+    in_path[v] = 1;
 
     arc_t * p = L->_heads[v];
     for (; p; p = p->next) {
-        if (!visited[p->adj]) {
-            _dfs(g, L, p->adj, visited, path);
+        int a = p->adj;
+        if (_scc[a]) {
+            continue;
+        }
+
+        if (!in_path[a]) {
+            _dfs(g, L, a, path, in_path);
+            _low[v] = std::min(_low[v], _low[a]);
         } else {
-            auto it = path.begin();
-            for (; it != path.end() && *it != p->adj; ++it)
-                ;;
-            for (; it != path.end(); ++it) {
-                std::cout << g->vex(*it) << ' ';
+            _low[v] = std::min(_low[v], _dfn[a]);
+            std::stack<int> tmp;
+            int t;
+            do {
+                t = path.top();
+                tmp.push(t);
+                path.pop();
+            } while (t != a);
+            while (!tmp.empty()) {
+                std::cout << g->vex(tmp.top()) << ' ';
+                path.push(tmp.top());
+                tmp.pop();
             }
             std::cout << '\n';
         }
     }
 
-    path.pop_back();
-    visited[v] = 0;
+    if (_low[v] == _dfn[v] && !_scc[v]) {
+        int t;
+        do {
+            t = _stk.top();
+            _stk.pop();
+            _scc[t] = 1;
+        } while (t != v);
+    }
+
+    path.pop();
+    in_path[v] = 0;
 }
 
 void print_all_simple_loop(graph_t * g)
@@ -48,18 +85,23 @@ void print_all_simple_loop(graph_t * g)
     int C = g->vex_count();
     list_t * L = g->get_structure();
 
-    std::vector<int> visited(C, 0), path;
+    _low.assign(C,0);
+    _dfn.assign(C,0);
+    _visited.assign(C,0);
+    _scc.assign(C,0);
 
+    std::stack<int> path;
+    std::vector<int> in_path(C, 0);
     std::vector<int> indeg = _get_indegree(L, C);
     bool go = false;
     for (int i = 0; i < C; ++i) {
         if (indeg[i] == 0) {
             go = true;
-            _dfs(g, L, i, visited, path);
+            _dfs(g, L, i, path, in_path);
         }
     }
 
     if (!go) {
-        _dfs(g, L, 0, visited, path);
+        _dfs(g, L, 0, path, in_path);
     }
 }
