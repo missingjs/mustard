466f12b9e8dcbd13d23e2faa2e137953f559ad48
Tang Qiming <tangqiming@tigerknows.com>
2016-06-25 22:40:24 +0800

    fix: output path in floyd

--- dsb/9/4/floyd.sample.cpp
+++ dsb/9/4/floyd.sample.cpp
@@ -11,14 +11,20 @@ void floyd_print_all_path(graph_t * g)
     mx_t & mx = *g->get_structure();
     mx_t M(C,C), path(C,C);
 
+    // path: if path[i][j] is connected, path[i][j] is the point after i
+
+    int MAX = graph::numeric_weight<int>::unconnected_value();
     for (int i = 0; i < C; ++i) {
         for (int j = 0; j < C; ++j) {
             M[i][j] = mx[i][j];
-            path[i][j] = -1;
+            if (mx[i][j] < MAX) {
+                path[i][j] = j;
+            } else {
+                path[i][j] = -1;
+            }
         }
     }
 
-    int MAX = graph::numeric_weight<int>::unconnected_value();
     for (int k = 0; k < C; ++k) {
         for (int i = 0; i < C; ++i) {
             for (int j = 0; j < C; ++j) {
@@ -28,33 +34,48 @@ void floyd_print_all_path(graph_t * g)
 
                 int x = M[i][k], y = M[k][j];
                 if (x < MAX && y < MAX && x + y < M[i][j]) {
-                    path[i][j] = k;
+                    path[i][j] = path[i][k];
                     M[i][j] = x + y;
                 }
             }
         }
     }
 
-    std::stack<int> stk;
     for (int i = 0; i < C; ++i) {
         for (int j = 0; j < C; ++j) {
-            if (i == j || M[i][j] == MAX) {
+            if (path[i][j] == -1) {
                 continue;
             }
 
-            stk.push(j);
-            int k = path[i][j];
-            while (k >= 0) {
-                stk.push(k);
-                k = path[i][k];
-            }
-            stk.push(i);
-
-            while (!stk.empty()) {
-                std::cout << g->vex(stk.top()) << ' ';
-                stk.pop();
+            int u = i;
+            while (u != -1) {
+                std::cout << g->vex(u) << ' ';
+                u = path[u][j];
             }
             std::cout << '\n';
         }
     }
+
+//    std::stack<int> stk;
+//    for (int i = 0; i < C; ++i) {
+//        for (int j = 0; j < C; ++j) {
+//            if (i == j || M[i][j] == MAX) {
+//                continue;
+//            }
+//
+//            stk.push(j);
+//            int k = path[i][j];
+//            while (k >= 0) {
+//                stk.push(k);
+//                k = path[i][k];
+//            }
+//            stk.push(i);
+//
+//            while (!stk.empty()) {
+//                std::cout << g->vex(stk.top()) << ' ';
+//                stk.pop();
+//            }
+//            std::cout << '\n';
+//        }
+//    }
 }
